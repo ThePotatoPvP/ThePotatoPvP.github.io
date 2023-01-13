28783d94fb8e0a6179f6d90a58d019e43682b505
ThePotatoPvP <corentin.sallin@gmail.com>
2023-01-13 11:17:23 +0100

    added hexa themes

--- games/hexapawn.js
+++ games/hexapawn.js
@@ -22,3 +22,63 @@ function allMoves(board, x, y) {
     }
 
 }
+
+
+//Theme moment
+const whiteSquares = document.querySelectorAll(".white-square")
+const blackSquares = document.querySelectorAll(".black-square")
+
+function toCleanTheme() {
+    whiteSquares.forEach(square => {
+       square.style.backgroundColor = "#ccc";
+       square.style.backgroundImage = "none";
+    });
+    blackSquares.forEach(square => {
+       square.style.backgroundColor = "transparent";
+       square.style.backgroundImage = "none";
+    });
+}
+
+function toGreenTheme() {
+    whiteSquares.forEach(square => {
+       square.style.backgroundColor = "palegreen";
+       square.style.backgroundImage = "none";
+    });
+    blackSquares.forEach(square => {
+       square.style.backgroundColor = "green";
+       square.style.backgroundImage = "none";
+    });
+}
+
+function toBlueTheme() {
+    whiteSquares.forEach(square => {
+       square.style.backgroundColor = "lightblue";
+       square.style.backgroundImage = "none";
+    });
+    blackSquares.forEach(square => {
+       square.style.backgroundColor = "blue";
+       square.style.backgroundImage = "none";
+    });
+}
+
+function toRedTheme() {
+    whiteSquares.forEach(square => {
+       square.style.backgroundColor = "red";
+       square.style.backgroundImage = "none";
+    });
+    blackSquares.forEach(square => {
+       square.style.backgroundColor = "darkred";
+       square.style.backgroundImage = "none";
+    });
+}
+
+function toPinkTheme() {
+    whiteSquares.forEach(square => {
+       square.style.backgroundColor = "lavenderblush";
+       square.style.backgroundImage = "none";
+    });
+    blackSquares.forEach(square => {
+       square.style.backgroundColor = "pink";
+       square.style.backgroundImage = "none";
+    });
+}
