4fc6b313e0b7f16e2497e6da7376b71da9465938
ThePotatoPvP <corentin.sallin@gmail.com>
2023-07-19 16:46:32 +0200

    Adding loading delay

--- scripts/fetch_exo.js
+++ scripts/fetch_exo.js
@@ -1,31 +1,33 @@
 const urlParams = new URLSearchParams(window.location.search);
 
-if (!urlParams.has('id')) {
-    nonValidExoPage();
-} else {
-    const id = urlParams.get('id').toString();
-    const exerciseFile = `exercises/${id}.json`;
+window.onload = function () {
+    if (!urlParams.has('id')) {
+        nonValidExoPage();
+    } else {
+        const id = urlParams.get('id').toString();
+        const exerciseFile = `exercises/${id}.json`;
 
-    fetch(exerciseFile)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(`Failed to load exercise ${id}`);
-            }
-            return response.json();
-        })
-        .then(data => {
-            const problem = data.problem.toString();
-            const hint = data.hint;
-            const solution = data.solution;
-            document.getElementById('problem').innerHTML = problem;
-            document.getElementById('hint').innerHTML = hint;
-            document.getElementById('solution').innerHTML = solution;
-            document.getElementById('exo_id').innerHTML = id;
-        })
-        .catch(error => {
-            console.error(`Failed to load exercise ${id}: ${error}`);
-            nonValidExoPage();
-        });
+        fetch(exerciseFile)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error(`Failed to load exercise ${id}`);
+                }
+                return response.json();
+            })
+            .then(data => {
+                const problem = data.problem.toString();
+                const hint = data.hint;
+                const solution = data.solution;
+                document.getElementById('problem').innerHTML = problem;
+                document.getElementById('hint').innerHTML = hint;
+                document.getElementById('solution').innerHTML = solution;
+                document.getElementById('exo_id').innerHTML = id;
+            })
+            .catch(error => {
+                console.error(`Failed to load exercise ${id}: ${error}`);
+                nonValidExoPage();
+            });
+    }
 }
 
 function nonValidExoPage() {
